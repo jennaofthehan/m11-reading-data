ebcd3b625118274b9255db136ce54649efa0a3a6
Jenna Han <jenna3@uw.edu>
2016-10-13 10:18:38 -0700

    class time

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -3,7 +3,19 @@
 // You'll have to wait for you page to load to assign events to the elements created in your index.html file
 $(function() {
     // Use $.get to read in your `data/students.csv` dataset: remember, you must be running a local server
+    $.get("data/students.csv", function(data,error){
+    	var parsedData = Papa.parse(data,{header:true}).data;
+    	console.log(parsedData);
+    	var table = $("<table>");
+    	for(i = 0; i<parsedData[0].length; i++){
+    		table.append($("<th>"));
+    	}
 
+    	for(i = 0; i<parsedData.length; i++){
+    		table.append($("<tr>"));
+    		
+    	}
+    })
         // Parse the data using Papa.parse
 
 
@@ -22,4 +34,4 @@ $(function() {
         // Select your `sandbox` section, and append your table to it
 
     });
-});
+
